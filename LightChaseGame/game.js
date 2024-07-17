1eee14117e874e9ce7f64475bf100898153dbe88
ashu <codewithpriyaa@gmail.com>
2024-07-17 23:24:23 +0530

    added animation and game logic

--- LightChaseGame/game.js
+++ LightChaseGame/game.js
@@ -1,53 +1,44 @@
-userClickedPattern=[userchoosenColor]
-var buttonColours=["red","blue","green","yellow"]
-var gamePattern=[]
-function nextSequence(){
-    var randomNumber=Math.floor(Math.random()*4) 
-    
 
-randomChosenColour=buttonColours[randomNumber]
-gamePattern.push(randomChosenColour)
-console.log(randomChosenColour)
-$("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100)
-var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-  audio.play();
-$("document").on("click",function(handler){
-    var userchoosenColor=push
-})
+var buttonColours = ["red", "blue", "green", "yellow"];
 
+var gamePattern = [];
+var userClickedPattern = [];
 
+$(".btn").click(function() {
 
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
 
-// $("#"+randomChosenColour).on("click",function(){
-//     $("#"+randomChosenColour).fadeOut().fadeIn()
-//     makeSound(randomChosenColour)
-    
-//})
+  playSound(userChosenColour);
 
+  animatePress(userChosenColour);
+});
+
+function nextSequence() {
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  
+
+  playSound(randomChosenColour);
 }
-nextSequence()
-// function makeSound(randomChosenColour){
-//     switch (randomChosenColour) {
-//         case ("blue"):
-//             var blue=new Audio("sounds/blue.mp3")
-//             blue.play()
-//             break;
-//         case("green"):
-//             var green= new Audio("sounds/green.mp3")
-//                 green.play()
-//             break;
-//         case("red"):
-//             var red =new Audio("sounds/red.mp3")
-//             red.play();
-//             break;
-//         case("yellow"):
-//            var yellow=new Audio("sounds/blue.mp3")
-//               yellow.play()
-//               break;
-//         default:
-//             var wrong=new Audio("sounds/wrong.mp3")
-//               wrong.play()
-//             break;
-//     }
-// }
-// nextSequence()
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+//1. Create a new function called animatePress(), it should take a single input parameter called currentColour.
+function animatePress(currentColor) {
+
+  //2. Use jQuery to add this pressed class to the button that gets clicked inside animatePress().
+  $("#" + currentColor).addClass("pressed");
+
+  //3. use Google/Stackoverflow to figure out how you can use Javascript to remove the pressed class after a 100 milliseconds.
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
